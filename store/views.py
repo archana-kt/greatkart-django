e8e571c4b5d71e63aced3424edfc241f5574d4a7
archana-kt <archanakt.cse@gmail.com>
2022-10-10 20:36:26 +0530

    store and carts functionality

--- store/views.py
+++ store/views.py
@@ -1,11 +1,18 @@
 #import imp
 #from unicodedata import category
+from ast import keyword
 from unicodedata import category
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.template import context
+from carts.models import CartItem
 
+from django.http import HttpResponse
 from category.models import Catergory
 from .models import Product
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 # Create your views here.
 
 
@@ -16,14 +23,20 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Catergory, slug=category_slug)
         products = Product.objects.filter(
-            category=categories, is_available=True)
+            category=categories, is_available=True).order_by('id')
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'Products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
@@ -33,11 +46,29 @@ def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(
             category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(
+            request), product=single_product).exists()
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
 
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(
+                Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    print(context)
+    return render(request, 'store/store.html', context)
