c158e9135d88df43c5e59b0b12d07c0ef8c858dc
archana-kt <archanakt.cse@gmail.com>
2022-10-16 19:25:22 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +134,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    50: 'critical',
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'archukalpana@gmail.com'
+EMAIL_HOST_PASSWORD = 'gqhnhymgktsttawh'
+EMAIL_USE_TLS = True
