6dacb811ac61d0416d927ac4f3d6f1fb865619d8
archana-kt <archanakt.cse@gmail.com>
2022-11-01 13:52:13 +0530

    product order and payments

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
 
+    path('orders/', include('orders.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
