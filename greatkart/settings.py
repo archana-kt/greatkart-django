cd247fa36fd8f9608d8f95134d3d67efe75b11d7
archana-kt <archanakt.cse@gmail.com>
2022-11-03 01:06:02 +0530

    review and rating system

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -144,5 +144,6 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'archukalpana@gmail.com'
-EMAIL_HOST_PASSWORD = 'gqhnhymgktsttawh'
+EMAIL_HOST_PASSWORD = 'hnpnturnjviifmrs'
+
 EMAIL_USE_TLS = True
--- store/models.py
+++ store/models.py
@@ -3,7 +3,8 @@ import django
 from django.db import models
 from category.models import Catergory
 from django.urls import reverse
-# Create your models here.
+from accounts.models import Account
+from django.db.models import Avg, Count
 
 
 class Product(models.Model):
@@ -23,6 +24,22 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(
+            product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(
+            product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
 
 
 class VariationManager(models.Manager):
@@ -51,3 +68,18 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
+
+
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
